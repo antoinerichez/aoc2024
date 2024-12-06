847ce84694d6e9d1830d55a8118cad7856be5ec7
Antoine RICHEZ <antoine.richez@forem.be>
2024-12-06 15:25:24 +0100

    feat: prep day5

---
+++ src/year2024/day05.rs
@@ -0,0 +1,13 @@
+﻿type Input = u32;
+
+pub fn parse(input: &str) -> Input {
+    input.chars().filter(|x| x.is_ascii_digit()).collect()
+}
+
+pub fn part1(input: &Input) -> u32 {
+    1
+}
+
+pub fn part2(input: &Input) -> u32 {
+    2
+}
